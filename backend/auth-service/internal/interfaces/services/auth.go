58b401bf37f8b2a49e31e2989f2b63ef82193b8a
BitCoinOffical <jlucehok228@gmail.com>
2026-07-16 17:52:11 +0500

    feat: register user service

--- backend/auth-service/internal/interfaces/services/auth.go
+++ backend/auth-service/internal/interfaces/services/auth.go
@@ -29,7 +29,7 @@ func NewAuthService(repo *repo.AuthRepo, tokens *jwtpkg.ManagerToken, sessions *
 	return &AuthService{repo: repo, tokens: tokens, sessions: sessions}
 }
 
-func (s *AuthService) Login(ctx context.Context, req dto.UsersLoginDTO) (*models.Tokens, error) {
+func (s *AuthService) LoginUser(ctx context.Context, req *dto.UsersLoginDTO) (*models.Tokens, error) {
 	user, err := s.repo.GetUserByEmail(ctx, req.Email)
 	if err != nil {
 		return nil, fmt.Errorf("s.repo.GetUserByEmail: %w", err)
@@ -49,6 +49,42 @@ func (s *AuthService) Login(ctx context.Context, req dto.UsersLoginDTO) (*models
 	}
 
 	if err := s.sessions.SaveToken(ctx, user.ID, refreshToken, RefreshTTL); err != nil {
+		return nil, fmt.Errorf("s.session.SaveToken: %w", err)
+	}
+
+	return &models.Tokens{
+		AccessToken:  accessToken,
+		RefreshToken: refreshToken,
+	}, nil
+}
+
+func (s *AuthService) RegisterUser(ctx context.Context, req *dto.UsersRegisterDTO) (*models.Tokens, error) {
+	newpass, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("bcrypt.GenerateFromPassword: %w", err)
+	}
+
+	user := models.User{
+		Email:        req.Email,
+		PasswordHash: newpass,
+	}
+
+	id, err := s.repo.SaveUser(ctx, &user)
+	if err != nil {
+		return nil, fmt.Errorf("s.repo.SaveUser: %w", err)
+	}
+
+	accessToken, err := s.tokens.GenerateToken(id, AccessTTL)
+	if err != nil {
+		return nil, fmt.Errorf("accessToken s.tokens.GenerateToken: %w", err)
+	}
+
+	refreshToken, err := s.tokens.GenerateToken(id, RefreshTTL)
+	if err != nil {
+		return nil, fmt.Errorf("refreshToken s.tokens.GenerateToken: %w", err)
+	}
+
+	if err := s.sessions.SaveToken(ctx, id, refreshToken, RefreshTTL); err != nil {
 		return nil, fmt.Errorf("s.session.SaveToken: %w", err)
 	}
 
