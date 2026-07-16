2f9ae03baa21f91d24367cf4d5bdf89cc8039d1c
BitCoinOffical <jlucehok228@gmail.com>
2026-07-16 17:48:48 +0500

    fix: user models

--- backend/auth-service/internal/domain/models/auth.go
+++ backend/auth-service/internal/domain/models/auth.go
@@ -15,7 +15,7 @@ type User struct {
 	ID           uuid.UUID `db:"id"`
 	Name         string    `db:"name"`
 	Email        string    `db:"email"`
-	PasswordHash string    `db:"password_hash"`
+	PasswordHash []byte    `db:"password_hash"`
 	Picture      string    `db:"picture"`
 	CreatedAt    time.Time `db:"created_at"`
 	UpdatedAt    time.Time `db:"updated_at"`
