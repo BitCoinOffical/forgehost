1afe7cd8d319b195ff0b4eae1638a89e4edd1fde
BitCoinOffical <jlucehok228@gmail.com>
2026-07-16 17:47:42 +0500

    feat: create logic register user

--- backend/auth-service/internal/api/handlers/auth.go
+++ backend/auth-service/internal/api/handlers/auth.go
@@ -12,6 +12,10 @@ import (
 	"go.uber.org/zap"
 )
 
+const (
+	userContextKey = "user_claims"
+)
+
 type AuthHandler struct {
 	logger  *zap.Logger
 	service *services.AuthService
@@ -28,23 +32,52 @@ func (h *AuthHandler) Login(c *gin.Context) {
 		return
 	}
 
-	tokens, err := h.service.Login(c.Request.Context(), req)
+	tokens, err := h.service.LoginUser(c.Request.Context(), &req)
 	if err != nil {
 		if errors.Is(err, domain.ErrNotFound) || errors.Is(err, domain.ErrInvalidCredentials) {
 			response.Unauthorized(c, err, "invalid credentials", h.logger)
 			return
 		}
-		response.InternalServerError(c, "failed to login", err, h.logger)
+		response.InternalServerError(c, err, "failed to login", h.logger)
 		return
 	}
 
 	c.JSON(http.StatusOK, tokens)
 }
 
+func (h *AuthHandler) Register(c *gin.Context) {
+	var req dto.UsersRegisterDTO
+	if err := c.ShouldBindBodyWithJSON(&req); err != nil {
+		response.BadRequest(c, err, "failed to register", h.logger)
+		return
+	}
+
+	if req.Password != req.PasswordRetry {
+		response.BadRequest(c, domain.ErrPasswordMismatch, "passwords do not match", h.logger)
+		return
+	}
+
+	tokens, err := h.service.RegisterUser(c.Request.Context(), &req)
+	if err != nil {
+		if errors.Is(err, domain.ErrEmailAlreadyExists) {
+			response.Conflict(c, err, "email already exists", h.logger)
+			return
+		}
+		response.InternalServerError(c, err, "failed to register", h.logger)
+		return
+	}
+
+	c.JSON(http.StatusCreated, tokens)
+}
+
 func (h *AuthHandler) GoogleLogin(c *gin.Context) {
 
 }
 
-func (h *AuthHandler) Register(c *gin.Context) {
-
+func (h *AuthHandler) Logout(c *gin.Context) {
+	value, exists := c.Get(userContextKey)
+	if !exists {
+		response.Unauthorized(c, domain.ErrInvalidCredentials, "not found value by key", h.logger)
+		return
+	}
 }
