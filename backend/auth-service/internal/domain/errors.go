ac7251613a0d2bda0ee4c4bbb9a812a5d6be987b
BitCoinOffical <jlucehok228@gmail.com>
2026-07-16 17:50:08 +0500

    feat: add new handlers errors

--- backend/auth-service/internal/domain/errors.go
+++ backend/auth-service/internal/domain/errors.go
@@ -5,3 +5,6 @@ import "errors"
 var ErrEmptyToken = errors.New("missing token")
 var ErrNotFound = errors.New("user not found")
 var ErrInvalidCredentials = errors.New("incorrect email or password")
+var ErrEmptyPayload = errors.New("empty payload")
+var ErrPasswordMismatch = errors.New("the passwords don't match")
+var ErrEmailAlreadyExists = errors.New("such user is already registered")
