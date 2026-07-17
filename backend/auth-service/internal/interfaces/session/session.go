dcf8697f3061b68fc9fb154577ccf0cbb3a60604
BitCoinOffical <jlucehok228@gmail.com>
2026-07-17 15:04:22 +0500

    add get token

--- backend/auth-service/internal/interfaces/session/session.go
+++ backend/auth-service/internal/interfaces/session/session.go
@@ -29,6 +29,15 @@ func (s *Session) SaveToken(ctx context.Context, id uuid.UUID, value string, Ref
 	return nil
 }
 
+func (s *Session) GetToken(ctx context.Context, id string) (string, error) {
+	key := fmt.Sprintf("%s%s", tokenKey, id)
+	value, err := s.rdb.Get(ctx, key).Result()
+	if err != nil {
+		return "", fmt.Errorf("s.rdb.Get: %w", err)
+	}
+	return value, nil
+}
+
 func (s *Session) DeleteToken(ctx context.Context, id string) error {
 	key := fmt.Sprintf("%s%s", tokenKey, id)
 	if err := s.rdb.Del(ctx, key).Err(); err != nil {
