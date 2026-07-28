a3b7be4cf51620294cf011a25d797e6a67ab7d29
BitCoinOffical <jlucehok228@gmail.com>
2026-07-29 00:04:33 +0500

    refactor: rename value

--- backend/auth-service/internal/api/handlers/services.go
+++ backend/auth-service/internal/api/handlers/services.go
@@ -1,12 +1,14 @@
 package handlers
 
 import (
+	rabbitqueue "BitCoinOffical/forgehost/auth-service/internal/interfaces/queue/rabbitMQ"
 	"BitCoinOffical/forgehost/auth-service/internal/interfaces/repo"
 	"BitCoinOffical/forgehost/auth-service/internal/interfaces/services"
 	"BitCoinOffical/forgehost/auth-service/internal/interfaces/session"
 	jwtpkg "BitCoinOffical/forgehost/auth-service/pkg/jwt"
 
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/rabbitmq/amqp091-go"
 	"github.com/redis/go-redis/v9"
 	"go.uber.org/zap"
 	"golang.org/x/oauth2"
@@ -16,10 +18,11 @@ type Services struct {
 	Service *services.AuthService
 }
 
-func NewServices(manager *jwtpkg.ManagerToken, rdb *redis.Client, pool *pgxpool.Pool, googleClientID string) *Services {
+func NewServices(manager *jwtpkg.ManagerToken, rdb *redis.Client, pool *pgxpool.Pool, WebGoogleClientID string, conn *amqp091.Connection, logger *zap.Logger) *Services {
+	queue := rabbitqueue.NewQueue(conn)
 	sessions := session.NewSession(rdb)
 	repo := repo.NewAuthRepo(pool)
-	service := services.NewAuthService(repo, manager, sessions, googleClientID)
+	service := services.NewAuthService(repo, manager, sessions, WebGoogleClientID, queue, logger)
 	return &Services{Service: service}
 }
 
