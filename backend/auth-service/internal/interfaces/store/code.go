d30488bb609a630b2fe0000a32b16ab410545904
BitCoinOffical <jlucehok228@gmail.com>
2026-08-04 23:03:57 +0500

    feat: added the ability to save the password reset code to redis

--- backend/auth-service/internal/interfaces/store/code.go
+++ backend/auth-service/internal/interfaces/store/code.go
@@ -11,7 +11,8 @@ import (
 )
 
 const (
-	codeKey = "code:"
+	codeKey  = "code:"
+	resetKey = "reset:"
 )
 
 type CodeStore struct {
@@ -28,6 +29,26 @@ func (s *CodeStore) SaveVerificationCode(ctx context.Context, randStr string, va
 		return fmt.Errorf("c.rdb.Set: %w", err)
 	}
 	return nil
+}
+
+func (s *CodeStore) SaveResetPasswordCode(ctx context.Context, randStr string, value int, expiration time.Duration) error {
+	key := fmt.Sprintf("%s%s", resetKey, randStr)
+	if err := s.rdb.Set(ctx, key, value, expiration).Err(); err != nil {
+		return fmt.Errorf("c.rdb.Set: %w", err)
+	}
+	return nil
+}
+
+func (s *CodeStore) GetResetPasswordCode(ctx context.Context, randStr string) (string, error) {
+	key := fmt.Sprintf("%s%s", resetKey, randStr)
+	val, err := s.rdb.Get(ctx, key).Result()
+	if err != nil {
+		if errors.Is(err, redis.Nil) {
+			return "", fmt.Errorf("s.rdb.Get: %w", domain.ErrNotFound)
+		}
+		return "", fmt.Errorf("s.rdb.Get: %w", err)
+	}
+	return val, nil
 }
 
 func (s *CodeStore) GetVerificationCode(ctx context.Context, randStr string) (string, error) {
