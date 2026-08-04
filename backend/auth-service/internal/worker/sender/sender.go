bf698f7bb7590522edc7114baf5cf5bdbf519784
BitCoinOffical <jlucehok228@gmail.com>
2026-08-04 23:03:01 +0500

    refactor(sender): The consumer was merged with the worker to simplify the logic

--- backend/auth-service/internal/worker/sender/sender.go
+++ backend/auth-service/internal/worker/sender/sender.go
@@ -1,34 +1,41 @@
 package sender
 
 import (
+	rabbitmq "BitCoinOffical/forgehost/auth-service/internal/adapters/RabbitMQ"
 	"BitCoinOffical/forgehost/auth-service/internal/adapters/email"
 	"BitCoinOffical/forgehost/auth-service/internal/domain"
 	"BitCoinOffical/forgehost/auth-service/internal/domain/dto"
-	"BitCoinOffical/forgehost/auth-service/internal/worker/connect"
+
 	"encoding/json"
 	"fmt"
 	"sync"
 	"time"
 
+	amqp "github.com/rabbitmq/amqp091-go"
+
 	"go.uber.org/zap"
 )
 
 const (
-	connectionTimeout  = 10
-	connectionAttempts = 5
+	attemptTTL    = 10000 //10sec
+	maxAttempts   = 5
+	prefetchCount = 1
+	prefetchSize  = 0
 )
 
 type Worker struct {
-	consumer *connect.Consumer
 	logger   *zap.Logger
 	rclient  *email.ResendClient
+	conn     *rabbitmq.ResilientConnection
+	TaskType string
 }
 
-func NewWorker(logger *zap.Logger, rclient *email.ResendClient, consumer *connect.Consumer) *Worker {
+func NewWorker(logger *zap.Logger, rclient *email.ResendClient, conn *rabbitmq.ResilientConnection, TaskType string) *Worker {
 	return &Worker{
 		logger:   logger,
 		rclient:  rclient,
-		consumer: consumer,
+		conn:     conn,
+		TaskType: TaskType,
 	}
 }
 
@@ -39,15 +46,64 @@ func (w *Worker) WorkerPool(worker int) <-chan error {
 	for num := range worker {
 		wg.Go(func() {
 			for {
-				consumer, err := w.consumer.StartConsumer()
+				ch, err := w.conn.NewChannel()
 				if err != nil {
-					errs <- fmt.Errorf("StartConsumer: %w", err)
-					time.Sleep(connectionTimeout * time.Second)
+					w.logger.Error("c.conn.NewChannel", zap.Error(err))
+					time.Sleep(time.Second)
 					continue
 				}
-				w.logger.Info("worker started", zap.Int("number", num+1))
 
-				for msg := range consumer.Msgs {
+				q, err := ch.QueueDeclare(
+					w.TaskType,
+					true,
+					false,
+					false,
+					false,
+					amqp.Table{
+						amqp.QueueTypeArg:  amqp.QueueTypeQuorum,
+						"x-message-ttl":    attemptTTL,
+						"x-delivery-limit": maxAttempts,
+					},
+				)
+				if err != nil {
+					ch.Close()
+					w.logger.Error("ch.QueueDeclare", zap.Error(err))
+					time.Sleep(time.Second)
+					continue
+				}
+
+				err = ch.Qos(
+					prefetchCount,
+					prefetchSize,
+					false,
+				)
+				if err != nil {
+					ch.Close()
+					w.logger.Error("ch.Qos", zap.Error(err))
+					time.Sleep(time.Second)
+					continue
+				}
+
+				msgs, err := ch.Consume(
+					q.Name,
+					"",
+					false,
+					false,
+					false,
+					false,
+					nil,
+				)
+				if err != nil {
+					ch.Close()
+					w.logger.Error("ch.Consume", zap.Error(err))
+					time.Sleep(time.Second)
+					continue
+				}
+
+				w.logger.Info("consumer started", zap.String("queue", w.TaskType))
+				w.logger.Info("worker started", zap.Int("number", num+1), zap.String("task", w.TaskType))
+
+				for msg := range msgs {
 					var body dto.RabbitQueueDTO
 					if err := json.Unmarshal(msg.Body, &body); err != nil {
 						errs <- fmt.Errorf("json.Unmarshal: %w", err)
@@ -62,7 +118,7 @@ func (w *Worker) WorkerPool(worker int) <-chan error {
 						continue
 					}
 
-					sentId, err := w.rclient.SendVerificationEmail([]string{body.Email}, body.Code)
+					sentId, err := w.rclient.SendCodeEmail([]string{body.Email}, body.Code, body.TitleSubject)
 					if err != nil {
 						errs <- fmt.Errorf("w.rclient.SendVerificationEmail: %w", err)
 						msg.Nack(false, true)
@@ -71,13 +127,12 @@ func (w *Worker) WorkerPool(worker int) <-chan error {
 					w.logger.Info("email sent successfully:", zap.Any("email id", sentId))
 					msg.Ack(false)
 				}
-				w.logger.Info("worker stoped", zap.Int("number", num+1))
-				if err := consumer.CloseConsumerChannel(); err != nil {
-					errs <- fmt.Errorf("consumer.CloseConsumerChannel: %w worker number %d", err, num+1)
-				}
+				ch.Close()
+				w.logger.Info("worker stoped", zap.Int("number", num+1), zap.String("task", w.TaskType))
 			}
 		})
 	}
+
 	go func() {
 		wg.Wait()
 		defer close(errs)
