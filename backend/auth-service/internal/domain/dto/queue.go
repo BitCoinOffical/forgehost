d537cc1eb19af9b85bab7a2b3c4fa8cbf098317d
BitCoinOffical <jlucehok228@gmail.com>
2026-08-09 16:58:24 +0500

    fix: remove TaskType field from RabbitQueueDTO

--- backend/auth-service/internal/domain/dto/queue.go
+++ backend/auth-service/internal/domain/dto/queue.go
@@ -8,6 +8,5 @@ type RabbitQueueDTO struct {
 	Email  string `json:"email"`
 
 	TitleSubject string    `json:"subject"`
-	TaskType     string    `json:"task_type"`
 	DispatchDate time.Time `json:"dispatch_date"`
 }
