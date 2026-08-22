bc1e1b1b306b09659912cc40ed379b0275814b64
BitCoinOffical <jlucehok228@gmail.com>
2026-08-22 23:18:00 +0500

    feat: implement CommentHandler with basic CRUD methods

---
+++ backend/social-service/internal/api/http/handlers/comments.go
@@ -0,0 +1,25 @@
+package handlers
+
+import "github.com/gin-gonic/gin"
+
+type CommentHandler struct {
+}
+
+func NewCommentHandler() *CommentHandler {
+	return &CommentHandler{}
+}
+func (h *CommentHandler) List(c *gin.Context) {
+
+}
+
+func (h *CommentHandler) Create(c *gin.Context) {
+
+}
+
+func (h *CommentHandler) Update(c *gin.Context) {
+
+}
+
+func (h *CommentHandler) Delete(c *gin.Context) {
+
+}
