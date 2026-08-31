076885276a075149560f9ba699d4c7c5e417a08e
BitCoinOffical <jlucehok228@gmail.com>
2026-08-31 17:09:24 +0500

    feat: add user_role type and update users table to include role column

--- backend/auth-service/internal/adapters/migrations/sql/000001_init.sql
+++ backend/auth-service/internal/adapters/migrations/sql/000001_init.sql
@@ -1,12 +1,15 @@
 -- +goose Up
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
+CREATE TYPE user_role AS ENUM ('user', 'admin');
+
 CREATE TABLE IF NOT EXISTS users(
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     name VARCHAR,
     email VARCHAR NOT NULL UNIQUE,
     password_hash VARCHAR,
     picture VARCHAR,
+    role user_role NOT NULL DEFAULT 'user',
     email_verified BOOLEAN NOT NULL DEFAULT false,
     email_banned BOOLEAN NOT NULL DEFAULT false,
     created_at TIMESTAMP DEFAULT NOW(),
