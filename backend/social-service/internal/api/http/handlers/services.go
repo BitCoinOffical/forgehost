f1da91019e1893312d63f2a765a325c130c484f9
BitCoinOffical <jlucehok228@gmail.com>
2026-09-04 00:06:51 +0500

    feat: update NewServices and NewHandlers to include PostsService

--- backend/social-service/internal/api/http/handlers/services.go
+++ backend/social-service/internal/api/http/handlers/services.go
@@ -1,27 +1,36 @@
 package handlers
 
 import (
+	"github.com/BitCoinOffical/forgehost/social-service/internal/intefaces/cache"
 	"github.com/BitCoinOffical/forgehost/social-service/internal/intefaces/repo"
 	"github.com/BitCoinOffical/forgehost/social-service/internal/intefaces/services"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/redis/go-redis/v9"
 	"go.uber.org/zap"
 )
 
 type Services struct {
 	profile *services.ProfileService
+	post    *services.PostsService
 }
 
-func NewServices(pool *pgxpool.Pool) *Services {
-	repo := repo.NewProfileRepo(pool)
-	profile := services.NewProfileService(repo)
-	return &Services{profile: profile}
+func NewServices(pool *pgxpool.Pool, rdb *redis.Client) *Services {
+	profrepo := repo.NewProfileRepo(pool)
+	profile := services.NewProfileService(profrepo)
+
+	postrdb := cache.NewCache(rdb)
+	postrepo := repo.NewPostsRepo(pool)
+	post := services.NewPostsService(postrepo, postrdb)
+	return &Services{profile: profile, post: post}
 }
 
 type Handlers struct {
 	Profile *ProfileHandler
+	Posts   *PostHandler
 }
 
-func NewHandlers(prof *services.ProfileService, logger *zap.Logger) *Handlers {
-	profile := NewProfileHandler(prof, logger)
-	return &Handlers{Profile: profile}
+func NewHandlers(srvc *Services, logger *zap.Logger) *Handlers {
+	profile := NewProfileHandler(srvc.profile, logger)
+	posts := NewPostHandler(srvc.post, logger)
+	return &Handlers{Profile: profile, Posts: posts}
 }
