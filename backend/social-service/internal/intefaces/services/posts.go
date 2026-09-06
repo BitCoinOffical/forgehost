919c2f72c4b71fe4ea9004d8085f9c65fa851732
BitCoinOffical <jlucehok228@gmail.com>
2026-09-06 18:52:37 +0500

    feat: add methods for topic retrieval, post creation, updating, reporting, and liking/unliking posts with improved error handling

--- backend/social-service/internal/intefaces/services/posts.go
+++ backend/social-service/internal/intefaces/services/posts.go
@@ -76,8 +76,45 @@ func (r *PostsService) GetGlobalPosts(ctx context.Context, id, cursor string) ([
 	return posts, nil
 }
 
-func (r *PostsService) GetPostById(ctx context.Context, id string) (*models.Post, error) {
-	post, err := r.repo.GetPostById(ctx, id)
+func (r *PostsService) GetTopics(ctx context.Context) ([]models.Topics, error) {
+	tpcs, err := r.repo.GetTopics(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("r.repo.GetTopics: %w", err)
+	}
+	return tpcs, nil
+}
+
+func (r *PostsService) CreatePost(ctx context.Context, req *dto.CreatePostDTO, id string) error {
+	post := models.Post{
+		TopicId:     req.TopicId,
+		UserID:      id,
+		ImageURL:    &req.ImageUrl,
+		Description: &req.Description,
+	}
+	if err := r.repo.CreatePost(ctx, &post); err != nil {
+		return fmt.Errorf("r.repo.CreatePost: %w", err)
+	}
+
+	return nil
+}
+
+func (r *PostsService) UpdatePost(ctx context.Context, req *dto.UpdatePostDTO, id string) (*models.Post, error) {
+	post := models.Post{
+		ID:          req.PostId,
+		TopicId:     req.TopicId,
+		ImageURL:    &req.ImageUrl,
+		Description: &req.Description,
+		UserID:      id,
+	}
+	res, err := r.repo.BuildUpdatePost(ctx, &post)
+	if err != nil {
+		return nil, fmt.Errorf("r.repo.BuildUpdatePost: %w", err)
+	}
+	return res, nil
+}
+
+func (r *PostsService) GetPostById(ctx context.Context, postId string) (*models.FeedPost, error) {
+	post, err := r.repo.GetPostById(ctx, postId)
 	if err != nil {
 		return nil, fmt.Errorf("r.repo.GetPostById: %w", err)
 	}
@@ -87,6 +124,40 @@ func (r *PostsService) GetPostById(ctx context.Context, id string) (*models.Post
 func (s *PostsService) ViewPost(ctx context.Context, id string) error {
 	if err := s.repo.ViewPost(ctx, id); err != nil {
 		return fmt.Errorf("s.repo.ViewPost: %w", err)
+	}
+	return nil
+}
+
+func (s *PostsService) ReportPost(ctx context.Context, req *dto.ReportDTO, userId, postId string) error {
+	report := models.PostReport{
+		UserId: userId,
+		PostId: postId,
+		Cause:  req.Cause,
+	}
+
+	if err := s.repo.ReportPost(ctx, &report); err != nil {
+		return fmt.Errorf("s.repo.ReportPost: %w", err)
+	}
+
+	return nil
+}
+
+func (s *PostsService) DeletePost(ctx context.Context, postId string, userId string) error {
+	if err := s.repo.DeletePost(ctx, postId, userId); err != nil {
+		return fmt.Errorf("s.repo.DeletePost: %w", err)
+	}
+	return nil
+}
+
+func (s *PostsService) LikePost(ctx context.Context, userId, postId string) error {
+	if err := s.repo.LikePost(ctx, userId, postId); err != nil {
+		return fmt.Errorf("s.repo.LikePost: %w", err)
+	}
+	return nil
+}
+func (s *PostsService) UnlikePost(ctx context.Context, userId, postId string) error {
+	if err := s.repo.UnlikePost(ctx, userId, postId); err != nil {
+		return fmt.Errorf("s.repo.UnlikePost: %w", err)
 	}
 	return nil
 }
