1ec26d13022206ffef431f4a855f643e7681ca5c
BitCoinOffical <jlucehok228@gmail.com>
2026-09-22 14:38:38 +0500

    fix: correct table name in unique index creation for profiles

--- backend/social-service/internal/adapters/migrations/sql/000002_init.sql
+++ backend/social-service/internal/adapters/migrations/sql/000002_init.sql
@@ -1,6 +1,6 @@
 -- +goose NO TRANSACTION
 -- +goose Up
-CREATE UNIQUE INDEX CONCURRENTLY idx_profile_username ON profile(username);
+CREATE UNIQUE INDEX CONCURRENTLY idx_profile_username ON profiles(username);
 
 -- +goose Down
 DROP INDEX CONCURRENTLY IF EXISTS idx_profile_username;
