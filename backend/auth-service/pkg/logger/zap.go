5ad8bf0574279594879aaec3d446e0cca9688947
BitCoinOffical <jlucehok228@gmail.com>
2026-09-22 15:23:33 +0500

    refactor: enhance NewLogger function to include logPath and configure file writer settings

--- backend/auth-service/pkg/logger/zap.go
+++ backend/auth-service/pkg/logger/zap.go
@@ -2,25 +2,46 @@ package loggerpkg
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/DeRuina/timberjack"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
-func NewLogger(level string) (*zap.Logger, error) {
+const (
+	maxSize    = 100
+	maxBackups = 5
+	maxAge     = 1
+)
+
+func NewLogger(level string, logPath string) (*zap.Logger, error) {
+	var encoderCfg zapcore.EncoderConfig
+	var lvl zapcore.Level
+
 	switch level {
 	case "prod":
-		prod, err := zap.NewProduction()
-		if err != nil {
-			return nil, fmt.Errorf("zap.NewProduction: %w", err)
-		}
-		return prod, nil
+		encoderCfg = zap.NewProductionEncoderConfig()
+		lvl = zap.InfoLevel
 	case "dev":
-		dev, err := zap.NewDevelopment()
-		if err != nil {
-			return nil, fmt.Errorf("zap.NewProduction: %w", err)
-		}
-		return dev, nil
+		encoderCfg = zap.NewDevelopmentEncoderConfig()
+		lvl = zap.DebugLevel
 	default:
 		return nil, fmt.Errorf("incorrect debug level: %s", level)
 	}
+
+	fileWriter := &timberjack.Logger{
+		Filename:    logPath,
+		MaxSize:     maxSize,
+		MaxBackups:  maxBackups,
+		MaxAge:      maxAge,
+		Compression: "gzip",
+	}
+
+	core := zapcore.NewTee(
+		zapcore.NewCore(zapcore.NewJSONEncoder(encoderCfg), fileWriter, lvl),
+		zapcore.NewCore(zapcore.NewConsoleEncoder(encoderCfg), zapcore.AddSync(os.Stdout), lvl),
+	)
+
+	return zap.New(core), nil
 }
