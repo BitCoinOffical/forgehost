226e45c0e01029b8b4a4b29ba43a7f9e6cfc52e8
BitCoinOffical <jlucehok228@gmail.com>
2026-09-23 16:33:49 +0500

    refactor: remove unused AuthService and related dependencies from auth.go

--- backend/auth-service/internal/interfaces/services/auth.go
+++ backend/auth-service/internal/interfaces/services/auth.go
@@ -1,16 +1,7 @@
 package services
 
 import (
-	rabbitqueue "github.com/BitCoinOffical/forgehost/auth-service/internal/interfaces/queue/rabbitMQ"
-	"github.com/BitCoinOffical/forgehost/auth-service/internal/interfaces/repo"
-	"github.com/BitCoinOffical/forgehost/auth-service/internal/interfaces/store"
-	"github.com/twmb/franz-go/pkg/kgo"
-
 	"time"
-
-	jwtpkg "github.com/BitCoinOffical/forgehost/auth-service/pkg/jwt"
-
-	"go.uber.org/zap"
 )
 
 const (
@@ -21,42 +12,3 @@ const (
 	codeSubject     = "Verification Code"
 	resetSubject    = "Password Reset Code"
 )
-
-type AuthService struct {
-	logger            *zap.Logger
-	client            *kgo.Client
-	tokens            *jwtpkg.ManagerToken
-	repo              *repo.AuthRepo
-	queue             *rabbitqueue.RabbitQueue
-	codeStore         *store.CodeStore
-	resendStore       *store.ResendStore
-	sessionStore      *store.SessionStore
-	userStore         *store.UserStore
-	WebgoogleClientID string
-}
-
-func NewAuthService(
-	repo *repo.AuthRepo,
-	tokens *jwtpkg.ManagerToken,
-	codeStore *store.CodeStore,
-	resendStore *store.ResendStore,
-	sessionStore *store.SessionStore,
-	userStore *store.UserStore,
-	WebgoogleClientID string,
-	queue *rabbitqueue.RabbitQueue,
-	logger *zap.Logger,
-	client *kgo.Client,
-) *AuthService {
-	return &AuthService{
-		repo:              repo,
-		tokens:            tokens,
-		codeStore:         codeStore,
-		resendStore:       resendStore,
-		sessionStore:      sessionStore,
-		userStore:         userStore,
-		WebgoogleClientID: WebgoogleClientID,
-		queue:             queue,
-		logger:            logger,
-		client:            client,
-	}
-}
