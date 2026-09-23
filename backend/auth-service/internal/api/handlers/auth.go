982902173e878b9e1089fd88e99cf807c0ade1cd
BitCoinOffical <jlucehok228@gmail.com>
2026-09-23 16:33:22 +0500

    refactor: remove unused imports and AuthHandler struct from auth handler

--- backend/auth-service/internal/api/handlers/auth.go
+++ backend/auth-service/internal/api/handlers/auth.go
@@ -1,25 +1,11 @@
 package handlers
 
 import (
-	"github.com/BitCoinOffical/forgehost/auth-service/internal/interfaces/services"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
-
-	"go.uber.org/zap"
-	"golang.org/x/oauth2"
 )
 
 var authRequestsTotal = promauto.NewCounter(prometheus.CounterOpts{
 	Name: "auth_requests_total",
 	Help: "Total number of auth requests",
 })
-
-type AuthHandler struct {
-	logger   *zap.Logger
-	oauthCfg *oauth2.Config
-	authsrvc *services.AuthService
-}
-
-func NewAuthHandler(logger *zap.Logger, authsrvc *services.AuthService, oauthCfg *oauth2.Config) *AuthHandler {
-	return &AuthHandler{logger: logger, authsrvc: authsrvc, oauthCfg: oauthCfg}
-}
