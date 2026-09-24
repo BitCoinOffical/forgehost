d00b2d5d87c689ccef3c57c8127331cfdbb75196
BitCoinOffical <jlucehok228@gmail.com>
2026-09-24 13:50:27 +0500

    refactor: update service interfaces to improve type usage and add PostsService

--- backend/social-service/internal/api/http/handlers/services.go
+++ backend/social-service/internal/api/http/handlers/services.go
@@ -1,6 +1,10 @@
 package handlers
 
 import (
+	"context"
+
+	"github.com/BitCoinOffical/forgehost/social-service/internal/domain/dto"
+	"github.com/BitCoinOffical/forgehost/social-service/internal/domain/models"
 	"github.com/BitCoinOffical/forgehost/social-service/internal/intefaces/cache"
 	"github.com/BitCoinOffical/forgehost/social-service/internal/intefaces/repo"
 	"github.com/BitCoinOffical/forgehost/social-service/internal/intefaces/services"
@@ -9,10 +13,45 @@ import (
 	"go.uber.org/zap"
 )
 
+type ProfileService interface {
+	GetProfileByID(ctx context.Context, id string) (*models.ProfileResponse, error)
+	SaveProfile(ctx context.Context, event *dto.UserProfileDTO) error
+	UpdateProfile(ctx context.Context, req *dto.UpdateProfileDTO, id string) (*models.Profile, error)
+	GetSubscriptions(ctx context.Context, id string) ([]models.Subscriptions, error)
+	GetSubscribers(ctx context.Context, id string) ([]models.Subscribes, error)
+	Subscribe(ctx context.Context, userId, targetId string) error
+	UnSubscribe(ctx context.Context, userId, targetId string) error
+	Report(ctx context.Context, userId, targetId string, req *dto.ReportDTO) error
+}
+
+type CommentsService interface {
+	ListComments(ctx context.Context, postId string) ([]models.FeedComments, error)
+	CreateComment(ctx context.Context, postId, userId string, comment *dto.CreateCommentDTO) error
+	UpdateComment(ctx context.Context, postId, userId, commentId string, comment *dto.UpdateCommentDTO) (*models.Comments, error)
+	DeleteComment(ctx context.Context, postId, userId, commentId string) error
+	ReportComment(ctx context.Context, userId, commentId string, comment *dto.ReportCommentDTO) error
+	LikeComment(ctx context.Context, userId, commentId string) error
+	UnlikeComment(ctx context.Context, userId, commentId string) error
+}
+
+type PostsService interface {
+	GetSubPosts(ctx context.Context, id string) ([]models.FeedPost, error)
+	GetGlobalPosts(ctx context.Context, id, cursor string) ([]models.FeedPost, error)
+	GetTopics(ctx context.Context) ([]models.Topics, error)
+	CreatePost(ctx context.Context, req *dto.CreatePostDTO, id string) error
+	UpdatePost(ctx context.Context, req *dto.UpdatePostDTO, id string) (*models.Post, error)
+	GetPostById(ctx context.Context, postId string) (*models.FeedPost, error)
+	ViewPost(ctx context.Context, id string) error
+	ReportPost(ctx context.Context, req *dto.ReportDTO, userId, postId string) error
+	DeletePost(ctx context.Context, postId string, userId string) error
+	LikePost(ctx context.Context, userId, postId string) error
+	UnlikePost(ctx context.Context, userId, postId string) error
+}
+
 type Services struct {
-	profile *services.ProfileService
-	post    *services.PostsService
-	coms    *services.CommentsService
+	profile ProfileService
+	post    PostsService
+	coms    CommentsService
 }
 
 func NewServices(pool *pgxpool.Pool, rdb *redis.Client) *Services {
